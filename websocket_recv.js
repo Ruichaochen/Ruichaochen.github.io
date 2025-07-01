86d1b7c8d3830c5b395e7e5e0a676817d53baf6d
Ruichao Chen <65166963+Ruichaochen@users.noreply.github.com>
2025-07-01 17:34:19 +0100

    Update websocket_recv.js

--- websocket_recv.js
+++ websocket_recv.js
@@ -40,6 +40,7 @@ function websocket_listen() {
             albumCoverElement.src = "https://i.scdn.co/image/" + message["metadata"]["image_url"].split(":")[2];
             displayElapsedTime(message["position"], totalDuration);
         }    
+        displayElapsedTime(Date.now() - startTime, totalDuration);
         if (isPlayingNow) {
             if (!isPlaying) {
                 startTime = Date.now() - currentPosition;
@@ -48,14 +49,10 @@ function websocket_listen() {
             }
         } else {
             if (isPlaying) {
-                displayElapsedTime(Date.now() - startTime, totalDuration);
                 clearInterval(timerInterval);
                 elapsedTime = Date.now() - startTime;
                 isPlaying = false;
             }
-        }
-        if (isPlayingNow) {
-            displayElapsedTime(Date.now() - startTime, totalDuration);
         }
     };
 
